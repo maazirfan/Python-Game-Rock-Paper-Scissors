b3a697431ca05b98b9b25b9246f88142e12ff140
Muhammad Maaz Irfan <muhammadmaazirfan@outlook.com>
2021-01-04 01:41:00 +0500

    Add files via upload

---
+++ Python Game Rock, Paper, Scissors.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  4 00:28:51 2021
+
+@author: Maaz Irfan
+"""
+from random import randint
+t= ["Rock","Paper","Scissors"]
+
+computer = t[randint(0, 2)]
+
+player = False
+
+while player == False:
+    player = input("Rock,Paper,Scissors?")
+    if player == computer:
+        print("Tie!")
+    elif player =="Rock":
+        if computer == "Paper":
+            print("You lose!", computer, "covers", player)
+        else:
+            print("You win!",player,"smashes",computer)
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lose!", computer, "cut", player)
+        else:
+            print("You win!", player, "covers", computer)
+    elif player== "Scissors":
+        
+        if computer == "Rock":
+            
+            print("You lose!", player, "cut", computer)
+    else:
+        
+        print("That's not a valid play. Check your spelling!")
+   
+        
